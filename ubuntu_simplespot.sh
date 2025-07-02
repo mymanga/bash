e913a5294f69cf42a1934c0a13769e7698e738bb
Barnabie Opiyo <barnabieo@gmail.com>
2025-07-02 09:45:53 +0300

    Nginx Fixes
    Removed predefined certbot settings in default site

--- ubuntu_simplespot.sh
+++ ubuntu_simplespot.sh
@@ -775,6 +775,9 @@ ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 cat > /etc/nginx/sites-available/default << EOL
 server {
 
+    listen 80;
+    listen [::]:80;
+    
     root /var/www/html/public;
     index index.php index.html index.htm index.nginx-debian.html;
 
@@ -792,27 +795,6 @@ server {
     location ~ /\.ht {
         deny all;
     }
-
-    listen [::]:443 ssl ipv6only=on; # managed by Certbot
-    listen 443 ssl; # managed by Certbot
-    ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem; # managed by Certbot
-    ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem; # managed by Certbot
-    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
-    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
-
-}
-server {
-    if (\$host = $DOMAIN) {
-        return 301 https://\$host\$request_uri;
-    } # managed by Certbot
-
-
-    listen 80;
-    listen [::]:80;
-
-    server_name $DOMAIN;
-    return 404; # managed by Certbot
-
 
 }
 EOL
